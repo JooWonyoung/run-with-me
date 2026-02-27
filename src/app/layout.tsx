f1d58fcefc1ff40dcd625c482c74ddba7cee8fb0
Joo <foafls@gmail.com>
2026-02-27 17:14:23 +0900

    feat: integrate Toaster component for enhanced notification display in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import QueryProvider from "@/providers/query-providers";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         <QueryProvider>{children}</QueryProvider>
+        <Toaster richColors position="bottom-right" />
       </body>
     </html>
   );
